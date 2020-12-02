def0b4d08ea6b9fe39a53df99ca50b23e3be9b0b
AyumiOsawa <a.osawa1002@gmail.com>
2020-12-02 17:07:10 +0200

    clean up

--- algorithmsAndDataStructures/roman_numeral_converter/index.js
+++ algorithmsAndDataStructures/roman_numeral_converter/index.js
@@ -6,51 +6,93 @@
 
 const convertToRoman = (num) => {
   let romanNumeral = '';
-  const base = [
-      { num: 1000, roman: 'M', repeat: 0, prefix: 0},
-      { num: 500, roman: 'D', repeat: 0, prefix: 0},
-      { num: 100, roman: 'C', repeat: 0, prefix: 0},
-      { num: 50, roman: 'L', repeat: 0, prefix: 0},
-      { num: 10, roman: 'X', repeat: 0, prefix: 0},
-      { num: 5, roman: 'V', repeat: 0, prefix: 0},
-      { num: 1, roman: 'I', repeat: 0, prefix: 0},
-      { num: 0, roman: '', repeat: 0, prefix: 0}
+  const base = [{
+      num: 1000,
+      roman: 'M',
+      repeat: 0,
+      prefix: 0
+    },
+    {
+      num: 500,
+      roman: 'D',
+      repeat: 0,
+      prefix: 0
+    },
+    {
+      num: 100,
+      roman: 'C',
+      repeat: 0,
+      prefix: 0
+    },
+    {
+      num: 50,
+      roman: 'L',
+      repeat: 0,
+      prefix: 0
+    },
+    {
+      num: 10,
+      roman: 'X',
+      repeat: 0,
+      prefix: 0
+    },
+    {
+      num: 5,
+      roman: 'V',
+      repeat: 0,
+      prefix: 0
+    },
+    {
+      num: 1,
+      roman: 'I',
+      repeat: 0,
+      prefix: 0
+    },
+    {
+      num: 0,
+      roman: '',
+      repeat: 0,
+      prefix: 0
+    }
   ];
 
- let currentNum = num;
- let baseIndex = 0;
+  let currentNum = num;
+  let baseIndex = 0;
 
-  while(currentNum >= 0 && baseIndex < base.length) {
-     const repeat = Math.floor(currentNum / base[baseIndex].num) || 0;
-     if (repeat === 4) {
-         base[baseIndex].prefix = 1;
-         base[baseIndex-1].repeat += 1;
-     } else {
-         base[baseIndex].repeat += repeat;
-     }
+  while (currentNum >= 0 && baseIndex < base.length) {
+    const repeat = Math.floor(currentNum / base[baseIndex].num) || 0;
+    if (repeat === 4) {
+      base[baseIndex].prefix = 1;
+      base[baseIndex - 1].repeat += 1;
+    } else {
+      base[baseIndex].repeat += repeat;
+    }
 
-     currentNum = currentNum - repeat * base[baseIndex].num;
-     baseIndex += 1;
+    currentNum = currentNum - repeat * base[baseIndex].num;
+    baseIndex += 1;
   };
 
   baseIndex = 0;
-  while(baseIndex < base.length) {
-     if (base[baseIndex].num.toString().includes('5')) {
-         if (base[baseIndex].repeat === 2) {
-             base[baseIndex].repeat = 0;
-             base[baseIndex - 1].repeat += 1;
-         }
-     }
-     baseIndex += 1;
+  while (baseIndex < base.length) {
+    if (base[baseIndex].num.toString().includes('5')) {
+      if (base[baseIndex].repeat === 2) {
+        base[baseIndex].repeat = 0;
+        base[baseIndex - 1].repeat += 1;
+      }
+    }
+    baseIndex += 1;
   }
 
   for (let i = 0; i < base.length; i++) {
-      if (base[i].prefix > 0) {
-         romanNumeral = romanNumeral.slice(0, romanNumeral.length - 1) + base[i].roman + romanNumeral.slice(romanNumeral.length - 1, romanNumeral.length);
-      };
-      romanNumeral += base[i].roman.repeat(base[i].repeat);
+    if (base[i].prefix > 0) {
+      romanNumeral = romanNumeral.slice(0, romanNumeral.length - 1)
+                      + base[i].roman
+                      + romanNumeral.slice(romanNumeral.length - 1,
+                        romanNumeral.length);
+    };
+    romanNumeral += base[i].roman.repeat(base[i].repeat);
   };
   return romanNumeral;
- }
+}
 
 module.exports = convertToRoman;
