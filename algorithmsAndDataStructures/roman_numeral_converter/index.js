5b4f232143cfb05671567b79b5553b1ae192e80a
AyumiOsawa <a.osawa1002@gmail.com>
2020-12-03 15:27:05 +0200

    reflect the comments from code review

--- algorithmsAndDataStructures/roman_numeral_converter/index.js
+++ algorithmsAndDataStructures/roman_numeral_converter/index.js
@@ -4,62 +4,60 @@
 //
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/roman-numeral-converter
 
+const letterNumberMapping = [{
+    num: 1000,
+    roman: 'M'
+  },
+  {
+    num: 500,
+    roman: 'D'
+  },
+  {
+    num: 100,
+    roman: 'C'
+  },
+  {
+    num: 50,
+    roman: 'L'
+  },
+  {
+    num: 10,
+    roman: 'X'
+  },
+  {
+    num: 5,
+    roman: 'V'
+  },
+  {
+    num: 1,
+    roman: 'I'
+  },
+  {
+    num: 0,
+    roman: ''
+  }
+];
+
 const convertToRoman = (num) => {
-  let romanNumeral = '';
-  const base = [{
-      num: 1000,
-      roman: 'M',
+  const base = letterNumberMapping.map(object => {
+    return {
+      ...object,
       repeat: 0,
       prefix: 0
-    },
-    {
-      num: 500,
-      roman: 'D',
-      repeat: 0,
-      prefix: 0
-    },
-    {
-      num: 100,
-      roman: 'C',
-      repeat: 0,
-      prefix: 0
-    },
-    {
-      num: 50,
-      roman: 'L',
-      repeat: 0,
-      prefix: 0
-    },
-    {
-      num: 10,
-      roman: 'X',
-      repeat: 0,
-      prefix: 0
-    },
-    {
-      num: 5,
-      roman: 'V',
-      repeat: 0,
-      prefix: 0
-    },
-    {
-      num: 1,
-      roman: 'I',
-      repeat: 0,
-      prefix: 0
-    },
-    {
-      num: 0,
-      roman: '',
-      repeat: 0,
-      prefix: 0
-    }
-  ];
+    };
+  });
 
-  let currentNum = num;
-  let baseIndex = 0;
+  // Check how many times each letters should be repeated by dividing the input
+  // number until the modulus becomes 0.
+  //
+  // repeat: the number of repetitions of roman letters
+  // prefix: the number of roman letters in front of other letters
+  //         (e.g. 'I' in 'IX' or 'IV')
+  let baseIndex, currentNum;
 
-  while (currentNum >= 0 && baseIndex < base.length) {
+  for (baseIndex = 0, currentNum = num;
+      baseIndex < base.length && currentNum >= 0;
+      baseIndex++) {
     const repeat = Math.floor(currentNum / base[baseIndex].num) || 0;
     if (repeat === 4) {
       base[baseIndex].prefix = 1;
@@ -67,28 +65,30 @@ const convertToRoman = (num) => {
     } else {
       base[baseIndex].repeat += repeat;
     }
-
     currentNum = currentNum - repeat * base[baseIndex].num;
-    baseIndex += 1;
   };
 
-  baseIndex = 0;
-  while (baseIndex < base.length) {
-    if (base[baseIndex].num.toString().includes('5')) {
-      if (base[baseIndex].repeat === 2) {
+  // Carry 1 to the next digit when the repetition of 5, 50, 500 is 2, and reset
+  // the repetition of 5, 50, 500 to 0.
+  for (let baseIndex = 0; baseIndex < base.length; baseIndex++) {
+    if (base[baseIndex].num.toString().includes('5') &&
+        base[baseIndex].repeat === 2) {
         base[baseIndex].repeat = 0;
         base[baseIndex - 1].repeat += 1;
-      }
-    }
-    baseIndex += 1;
-  }
+    };
+  };
+
+  // Put the roman letters together.
+  // Prefix should be inserted at the second position from the last in the
+  // current romanNumeral.
+  let romanNumeral = '';
 
   for (let i = 0; i < base.length; i++) {
     if (base[i].prefix > 0) {
-      romanNumeral = romanNumeral.slice(0, romanNumeral.length - 1)
-                      + base[i].roman
-                      + romanNumeral.slice(romanNumeral.length - 1,
-                        romanNumeral.length);
+      romanNumeral = romanNumeral.slice(0, romanNumeral.length - 1) +
+        base[i].roman +
+        romanNumeral.slice(romanNumeral.length - 1,
+          romanNumeral.length);
     };
     romanNumeral += base[i].roman.repeat(base[i].repeat);
   };
