bdcd1d682a5783c18fadd027ef1742b8902a9bc9
AyumiOsawa <a.osawa1002@gmail.com>
2020-12-09 13:05:48 +0200

    depth first search

---
+++ otherAlgorithmQuizes/depth_first_search/index.js
@@ -0,0 +1,68 @@
+/**
+ * Write a fucntion that takes:
+ * - graph: A 2D array representing an graph adjancency matrix (https://www.javatpoint.com/graph-theory-graph-representations#:~:text=In%20graph%20theory%2C%20a%20graph,to%20it%20by%20an%20edge).)
+ * - start: Int reference to an initial vertex
+ * - end: Int reference to an end vertex
+ * And returns true iff there is a path between start and end in the graph defined by graph
+ **/
+
+// MEMO:
+// row (index of first level array) -> from
+// column (index of second level arrays) -> to
+
+function checkPath(graph, start, end) {
+  let route = [];
+  let arrived = false;
+
+  const lookForTheNextRoute = (currentRow) => {
+    const activeNodes = [];
+    console.log('inside lookForTheNextRoute()');
+    // if current node is an ending node, return true.
+    if (currentRow === end) {
+      arrived = true;
+      console.log('this is the goal. I am at', currentRow);
+      return;
+    } else if (graph[currentRow].includes(1)) { // if there are route(s) to anothre node(s), recursively use this function.
+        console.log('not an end: node', currentRow);
+        for (let node = 0; node < graph[currentRow].length; node++) {
+          if(arrived) {
+            break;
+          } else if (graph[currentRow][node] === 1) {
+            console.log(`connection found from ${currentRow} to ${node}`);
+            lookForTheNextRoute(node);
+          };
+        };
+    } else { // if there is no rounte to another node(i.e. deadend), reset the route.
+      console.log('deadend. resetting the log.');
+      route = [];
+    };
+  };
+
+  lookForTheNextRoute(start);
+
+	return arrived;
+}; // end of function
+
+let graphA = [
+	[0,1,1,0,0],
+  [0,0,0,1,0],
+  [0,0,0,0,1],
+  [0,0,0,0,0],
+  [0,1,0,0,0]
+]
+
+let testCount = 0;
+
+function test(output, result) {
+	if (output == result) {
+  	console.log(testCount++ + " Passed.")
+  } else {
+  	console.log(testCount++ + " Failed. Returned " + output + " but expected " + result);
+  }
+}
+
+test(checkPath(graphA, 0, 1), true); // True 0 -> 1
+test(checkPath(graphA, 1, 0), false); // False
+test(checkPath(graphA, 0, 3), true); // True 0 -> 1 -> 3
+test(checkPath(graphA, 0, 4), true); // True 0 -> 2 -> 4
+test(checkPath(graphA, 4, 2), false); // Flase
