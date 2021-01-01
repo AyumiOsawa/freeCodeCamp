87be1c3a26d695de412effa8cb6b46fd12a619c0
AyumiOsawa <a.osawa1002@gmail.com>
2021-01-01 12:45:52 +0200

    find the minimum and maximum value in a binary search tree

--- coding_interview_prep/data_structures/find_the_minimum_and_maximum_value_in_a_binary_search_tree/index.js
+++ coding_interview_prep/data_structures/find_the_minimum_and_maximum_value_in_a_binary_search_tree/index.js
@@ -21,9 +21,36 @@ function BinarySearchTree() {
     this.left = null;
     this.right = null;
   }
-  // Only change code below this line
 
-  // Only change code above this line
+  this.findMin = function() {
+    if(this.root === null) {
+      return null;
+    };
+
+    let node = this.root;
+    let min;
+
+    while(node !== null) {
+      min = node.value;
+      node = node.left;
+    };
+    return min;
+  };
+
+  this.findMax = function() {
+    if(this.root === null) {
+      return null;
+    };
+
+    let node = this.root;
+    let max;
+
+    while(node !== null) {
+      max = node.value;
+      node = node.right;
+    };
+    return max;
+  };
 };
 
 module.exports = BinarySearchTree;
