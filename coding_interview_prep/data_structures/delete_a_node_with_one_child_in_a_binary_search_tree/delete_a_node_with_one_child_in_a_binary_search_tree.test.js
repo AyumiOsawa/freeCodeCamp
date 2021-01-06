f91a9db03c85991aa19f1b2835147a2041bd35d4
AyumiOsawa <a.osawa1002@gmail.com>
2021-01-06 14:19:07 +0200

    test added

---
+++ coding_interview_prep/data_structures/delete_a_node_with_one_child_in_a_binary_search_tree/delete_a_node_with_one_child_in_a_binary_search_tree.test.js
@@ -0,0 +1,108 @@
+const {
+  displayTree,
+  Node,
+  BinarySearchTree
+} = require('./index');
+const exampleBST = new BinarySearchTree();
+const oneNodeBST = new BinarySearchTree();
+
+describe(`The BinarySearchTree class should be able to delete nodes with one
+          child.`,
+          () => {
+            test(`The BinarySearchTree data structure should exist.`,
+                  () => {
+                    expect(exampleBST)
+                    .toBeInstanceOf(BinarySearchTree)
+                    expect(exampleBST.root)
+                    .toBeNull();
+                  });
+
+            test(`The binary search tree should have a method called remove.`,
+                  () => {
+                    const spyRemove = jest
+                                      .spyOn(exampleBST,
+                                            'remove')
+                                      .mockImplementation(() => {
+                                             return 'Remove';
+                                           });
+                    const spyRemoveResult = exampleBST.remove();
+
+                    expect(spyRemove)
+                    .toHaveBeenCalled();
+                    expect(spyRemoveResult)
+                    .toEqual('Remove');
+                    spyRemove.mockRestore();
+                  });
+
+            test(`Trying to remove an element that does not exist should return
+                  null.`,
+                  () => {
+                    const node1 = new Node(8);
+                    const node2 = new Node(3);
+                    const node3 = new Node(10);
+                    const node4 = new Node(4);
+
+                    node1.left = node2;
+                    node1.right = node3;
+                    node2.right = node4;
+                    exampleBST.root = node1;
+
+                    expect(exampleBST.remove(2))
+                    .toBeNull();
+                  });
+
+            test(`If the root node has no children, deleting it should set the
+                  root to null.`,
+                  () => {
+                    const node5 = new Node(8);
+
+                    oneNodeBST.root = node5;
+                    oneNodeBST.remove(8);
+
+                    expect(oneNodeBST.root)
+                    .toBeNull();
+                  });
+
+            test(`The remove method should remove leaf nodes from the tree.`,
+                  () => {
+                    exampleBST.remove(10);
+
+                    expect(exampleBST.root.right)
+                    .toBeNull();
+
+                    exampleBST.remove(4);
+
+                    expect(exampleBST.root.left.right)
+                    .toBeNull();
+                  });
+
+            test(`The remove method should remove nodes with one child.`,
+                  () => {
+                    const node1 = new Node(8);
+                    const node2 = new Node(3);
+                    const node3 = new Node(10);
+                    const node4 = new Node(4);
+
+                    node1.left = node2;
+                    node1.right = node3;
+                    node2.right = node4;
+                    exampleBST.root = node1;
+                    exampleBST.remove(3);
+
+                    expect(exampleBST.root.value)
+                    .toEqual(8);
+                    expect(exampleBST.root.left.value)
+                    .toEqual(4);
+                    expect(exampleBST.root.right.value)
+                    .toEqual(10);
+                  });
+
+            test(`Removing the root in a tree with two nodes should set the
+                  second to be the root.`,
+                  () => {
+                    exampleBST.remove(8);
+
+                    expect(exampleBST.root.value)
+                    .toEqual(10);
+                  });
+          })
---
+++ coding_interview_prep/data_structures/delete_a_node_with_one_child_in_a_binary_search_tree/index.js
@@ -0,0 +1,73 @@
+// DELETE A NODE WITH ONE CHILD IN A BINARY SEARCH TREE
+//
+// We've provided some code in our remove method that accomplishes the tasks
+// from the last challenge. We find the target to delete and its parent and
+// define the number of children the target node has. Let's add the next case
+// here for target nodes with only one child. Here, we'll have to determine if
+// the single child is a left or right branch in the tree and then set the
+// correct reference in the parent to point to this node. In addition, let's
+// account for the case where the target is the root node (this means the
+// parent node will be null). Feel free to replace all the starter code with
+// your own as long as it passes the tests.
+//
+// https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/delete-a-node-with-one-child-in-a-binary-search-tree
+
+var displayTree = tree => console.log(JSON.stringify(tree, null, 2));
+function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+function BinarySearchTree() {
+  this.root = null;
+  this.remove = function(value) {
+    if (this.root === null) {
+      return null;
+    }
+    var target;
+    var parent = null;
+    // Find the target value and its parent
+    (function findValue(node = this.root) {
+      if (value == node.value) {
+        target = node;
+      } else if (value < node.value && node.left !== null) {
+        parent = node;
+        return findValue(node.left);
+      } else if (value < node.value && node.left === null) {
+        return null;
+      } else if (value > node.value && node.right !== null) {
+        parent = node;
+        return findValue(node.right);
+      } else {
+        return null;
+      }
+    }.bind(this)());
+    if (target === null) {
+      return null;
+    }
+    // Count the children of the target to delete
+    var children =
+      (target.left !== null ? 1 : 0) + (target.right !== null ? 1 : 0);
+    // Case 1: Target has no children
+    if (children === 0) {
+      if (target == this.root) {
+        this.root = null;
+      } else {
+        if (parent.left == target) {
+          parent.left = null;
+        } else {
+          parent.right = null;
+        }
+      }
+    }
+    // Case 2: Target has one child
+    // Only change code below this line
+  };
+};
+
+module.exports = {
+  displayTree,
+  Node,
+  BinarySearchTree
+};
