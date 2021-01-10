31b1d3ffd85c6eccf591ccc9d0104230472c69ed
AyumiOsawa <a.osawa1002@gmail.com>
2021-01-10 21:49:24 +0100

    initial project setup

---
+++ front-end_library_project/drum_machine/gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+
+gulp.task("sass", function(){
+  gulp.src("./src/sass/**/*.sass")
+  .pipe(sass({outputStyle: "expanded"}))
+  .pipe(gulp.dest("./public"));
+});
+
+gulp.task("babel", function(){
+  gulp.src("./src/js/**/*.js")
+  .pipe(babel())
+  .pipe(uglify())
+  .pipe(rename({ extname: '.min.js' }))
+  .pipe(gulp.dest("./public"));
+});
