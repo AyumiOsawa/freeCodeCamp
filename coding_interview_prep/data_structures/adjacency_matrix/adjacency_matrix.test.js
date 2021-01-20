26e57806ffb2ff5ffa12c7acaba5dab217656934
AyumiOsawa <a.osawa1002@gmail.com>
2021-01-20 17:46:52 +0100

    adjacency matrix

--- coding_interview_prep/data_structures/adjacency_matrix/adjacency_matrix.test.js
+++ coding_interview_prep/data_structures/adjacency_matrix/adjacency_matrix.test.js
@@ -4,39 +4,39 @@ describe(`A graph with 5 nodes should be represented as a adjacency matrix.`,
           () => {
             test(`undirectedAdjList should only contain five nodes.`,
                   () => {
-                    expect(undirectedAdjList.length)
-                    .toEqual(5)
+                    expect(adjMatUndirected)
+                    .toHaveLength(5);
                   });
 
             test(`There should be an edge between the first and fourth node.`,
                   () => {
-                    expect(undirectedAdjList[0])
-                    .toContain(3);
-                    expect(undirectedAdjList[3])
-                    .toContain(0);
+                    expect(adjMatUndirected[0][3])
+                    .toEqual(1);
+                    expect(adjMatUndirected[3][0])
+                    .toEqual(1);
                   });
 
             test(`There should be an edge between the first and third node.`,
                   () => {
-                    expect(undirectedAdjList[0])
-                    .toContain(2);
-                    expect(undirectedAdjList[2])
-                    .toContain(0);
+                    expect(adjMatUndirected[0][2])
+                    .toEqual(1);
+                    expect(adjMatUndirected[2][0])
+                    .toEqual(1);
                   });
 
             test(`There should be an edge between the third and fifth node.`,
                   () => {
-                    expect(undirectedAdjList[2])
-                    .toContain(4);
-                    expect(undirectedAdjList[4])
-                    .toContain(2);
+                    expect(adjMatUndirected[2][4])
+                    .toEqual(1);
+                    expect(adjMatUndirected[4][2])
+                    .toEqual(1);
                   });
 
             test(`There should be an edge between the fourth and fifth node.`,
                   () => {
-                    expect(undirectedAdjList[3])
-                    .toContain(5);
-                    expect(undirectedAdjList[5])
-                    .toContain(3);
+                    expect(adjMatUndirected[3][4])
+                    .toEqual(1);
+                    expect(adjMatUndirected[4][3])
+                    .toEqual(1);
                   });
           });
--- coding_interview_prep/data_structures/adjacency_matrix/index.js
+++ coding_interview_prep/data_structures/adjacency_matrix/index.js
@@ -8,6 +8,12 @@
 //
 // https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/adjacency-matrix
 
-var adjMatUndirected = [];
+const adjMatUndirected = [
+    [0, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [0, 0, 1, 1, 0]
+];
 
 module.exports = adjMatUndirected;
