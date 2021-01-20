6e5fe2645447a3ff5ec62ab732ef28ae8b6a0b36
AyumiOsawa <a.osawa1002@gmail.com>
2021-01-20 18:28:50 +0100

    test added

---
+++ coding_interview_prep/data_structures/incidence_matrix/incidence_matrix.test.js
@@ -0,0 +1,47 @@
+const incMatUndirected = require('./index');
+
+describe(`An undirected graph of 5 nodes iwth 4 edges should be represented
+          cocrrectly in an incident matrix`,
+          () => {
+            test(`incMatUndirected should only contain five nodes.`,
+                  () => {
+                    expect(incMatUndirected)
+                    .toHaveLength(5);
+                  });
+
+            test(`There should be a first edge between the first and second
+                  node.`,
+                  () => {
+                    expect(incMatUndirected[0][0])
+                    .toEqual(1);
+                    expect(incMatUndirected[1][0])
+                    .toEqual(1);
+                  });
+
+            test(`There should be a second edge between the second and third
+                  node.`,
+                  () => {
+                    expect(incMatUndirected[1][1]])
+                    .toEqual(1);
+                    expect(incMatUndirected[2][1])
+                    .toEqual(1);
+                  });
+
+            test(`There should be a third edge between the third and fifth
+                  node.`,
+                  () => {
+                    expect(incMatUndirected[2][2])
+                    .toEqual(1);
+                    expect(incMatUndirected[4][2])
+                    .toEqual(1);
+                  });
+
+            test(`There should be a fourth edge between the second and fourth
+                  node.`,
+                  () => {
+                    expect(incMatUndirected[1][3])
+                    .toEqual(1);
+                    expect(incMatUndirected[3][3])
+                    .toEqual(1);
+                  });
+          });
---
+++ coding_interview_prep/data_structures/incidence_matrix/index.js
@@ -0,0 +1,18 @@
+// INCIDENCE MATRIX
+//
+// Create an incidence matrix of an undirected graph with five nodes and four
+// edges. This matrix should be in a multi-dimensional array.
+//
+// These five nodes have the following relationships. The first edge is between
+// the first and second node. The second edge is between the second and third
+// node. The third edge is between the third and fifth node. The fourth edge is
+// between the fourth and second node. All edge weights are one and the edge
+// order matters.
+//
+// https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/incidence-matrix
+
+const incMatUndirected = [
+
+];
+
+module.exports = incMatUndirected;
