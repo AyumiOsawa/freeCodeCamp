f0de437490c9e82a483a5c7ec92c4ceda47dd40c
AyumiOsawa <a.osawa1002@gmail.com>
2021-01-23 21:40:20 +0100

    required elements added

--- front-end_library_project/calculator/src/App.js
+++ front-end_library_project/calculator/src/App.js
@@ -1,56 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+// import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <div class="display" id="display">0</div>
+      <div class='key__container'>
+        <div className="colmn__numbers">
+          <div className="key key__number" id="nine">9</div>
+          <div className="key key__number" id="eight">8</div>
+          <div className="key key__number" id="seven">7</div>
+          <div className="key key__number" id="six">6</div>
+          <div className="key key__number" id="five">5</div>
+          <div className="key key__number" id="four">4</div>
+          <div className="key key__number" id="three">3</div>
+          <div className="key key__number" id="two">2</div>
+          <div className="key key__number" id="one">1</div>
+          <div className="key key__number" id="zero">0</div>
+        </div>
+        <div className="column__side">
+          <div className="key key__control" id="equal">=</div>
+          <div className="key key__control" id="add">+</div>
+          <div className="key key__control" id="subtract">-</div>
+          <div className="key key__control" id="multiply">X</div>
+          <div className="key key__control" id="divide">/</div>
+          <div className="key key__control" id="decimal">.</div>
+          <div className="key key__control" id="clear">AC</div>
+        </div>
+      </div>
     </div>
   );
 }
