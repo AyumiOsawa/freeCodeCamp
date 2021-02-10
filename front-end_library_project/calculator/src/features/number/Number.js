b255cd43c4ada0839c0cb11b6dd414646310edcc
AyumiOsawa <a.osawa1002@gmail.com>
2021-02-10 18:13:16 +0100

    clicking animation added

--- front-end_library_project/calculator/src/features/number/Number.js
+++ front-end_library_project/calculator/src/features/number/Number.js
@@ -2,38 +2,59 @@ import React from 'react';
 import './Number.css';
 
 export function Number() {
+  const keyNumberNames = [
+                          'zero',
+                          'one',
+                          'two',
+                          'three',
+                          'four',
+                          'five',
+                          'six',
+                          'seven',
+                          'eight',
+                          'nine'
+                          ];
+  const reversedKeyNumNames = keyNumberNames.reverse();
+
+  const handleClick = (event) => {
+    console.log('clicked');
+  }
+
   return (
     <div className="colmn__numbers">
-      <div className="key key_wrapper__number">
-        <div className="key__number" id="nine">9</div>
-      </div>
-      <div className="key key_wrapper__number">
-        <div className="key__number" id="eight">8</div>
-      </div>
-      <div className="key key_wrapper__number">
-        <div className="key__number" id="seven">7</div>
-      </div>
-      <div className="key key_wrapper__number">
-        <div className="key__number" id="six">6</div>
-      </div>
-      <div className="key key_wrapper__number">
-        <div className="key__number" id="five">5</div>
-      </div>
-      <div className="key key_wrapper__number">
-        <div className="key__number" id="four">4</div>
-      </div>
-      <div className="key key_wrapper__number">
-        <div className="key__number" id="three">3</div>
-      </div>
-      <div className="key key_wrapper__number">
-        <div className="key__number" id="two">2</div>
-      </div>
-      <div className="key key_wrapper__number">
-        <div className="key__number" id="one">1</div>
-      </div>
-      <div className="key key_wrapper__number key__zero">
-        <div className="key__number" id="zero">0</div>
-      </div>
+      {reversedKeyNumNames.map((numberName, index) => {
+        return (
+          numberName === 'zero' ?
+          (
+            <div
+              className="key key_wrapper__number key__zero"
+              key={9 - index}
+            >
+              <div
+                className="key__number"
+                id={numberName}
+                onClick={() => handleClick}
+              >
+                {9 - index}
+              </div>
+            </div>
+          ) :
+          (
+            <div
+              className="key key_wrapper__number"
+              key={9 - index}
+            >
+              <div
+                className="key__number"
+                id={numberName}
+                onClick={() => handleClick}
+              >
+                {9 - index}
+              </div>
+            </div>
+          )
+        );
+      })}
     </div>
   );
 
