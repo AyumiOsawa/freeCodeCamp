2fc4a914a3019530de06abcff7547830dcdc70c1
AyumiOsawa <a.osawa1002@gmail.com>
2021-02-15 12:46:25 +0100

    prevent multiple decimal point inputs in one number

--- front-end_library_project/calculator/src/features/number/numberSlice.js
+++ front-end_library_project/calculator/src/features/number/numberSlice.js
@@ -23,54 +23,58 @@ const inputSlice = createSlice({
     isFloat: false
   },
   reducers: {
-    input: ({inputs, isFloat}, action) => {
-      let newInputs = [action.payload];
+    input: (state, action) => {
+      let {inputs, isFloat} = state;
+      let newInput = action.payload;
       const lastInput = inputs[inputs.length - 1];
       const secondLastInput = inputs[inputs.length - 2];
-
+      let newStateInputs = [action.payload]
+      let newStateIsFloat = isFloat;
       // A decimal point can be added only once to one number
-      if (newInputs[0] === '.' && isFloat) {
-        newInputs = [];
-      } else if (newInputs[0] === '.' && !isFloat) {
-        isFloat = true;
+      if (/\./.test(newInput) && isFloat) {
+        newStateInputs = [];
+      } else if (/\./.test(newInput) && !isFloat) {
+        newStateIsFloat = true;
       }
-      // Folat number input mode is reset when a new operand is given
-      if(/[\+\*\=\/]/.test(newInputs[0])) {
-        isFloat = false;
+      // Float number input mode is reset when a new operand is given
+      if(/[\+\-\*\=\/]/.test(newInput)) {
+        newStateIsFloat = false;
       }
       // Add 0 to the front if the first input was a decimal point
-      if(/\./.test(newInputs[0]) &&
+      if(/\./.test(newInput) &&
          inputs.length === 0) {
-        newInputs = ['0', '.'];
+        newStateInputs = ['0', '.'];
+      }
+      // Minus can follow zero or one operand except the decimal point
+      const doesDecimalPointPrecede = /[\.]/.test(lastInput);
+      const areTheLast2InputsOperands = /[\.\+\-\*\=\/]/.test(lastInput) &&
+                                        /[\.\+\-\*\=\/]/.test(secondLastInput);
+      const shouldRejectMinusInput = areTheLast2InputsOperands ||
+                                     doesDecimalPointPrecede;
+      if(/\-/.test(newInput) && shouldRejectMinusInput) {
+        newStateInputs = [];
       }
       // Replace the latest input with the new input, if multiple operands were
       // given in a row.
       // Replace the second latest input with the new input and delete the last
-      // input, if the second is also an operand.
-      if (/[\.\+\*\=\/]/.test(newInputs[0]) &&
-          /[\.\+\-\*\=\/]/.test(lastInput)  &&
-          /[\.\+\-\*\=\/]/.test(secondLastInput)) {
-        inputs[inputs.length - 2] = newInputs[0];
+      // input, if the second lastest input is also an operand.
+      const isTheLastInputOperand = /[\.\+\-\*\=\/]/.test(lastInput);
+      if (/[\.\+\*\=\/]/.test(newInput) && areTheLast2InputsOperands) {
+        inputs[inputs.length - 2] = newInput;
         inputs.pop();
-        newInputs = [];
-      } else if(/[\.\+\*\=\/]/.test(newInputs[0]) &&
-                /[\.\+\-\*\=\/]/.test(lastInput)) {
-        inputs[inputs.length - 1] = newInputs[0];
-        newInputs = [];
+        newStateInputs = [];
+      } else if(/[\.\+\*\=\/]/.test(newInput) && isTheLastInputOperand) {
+        inputs[inputs.length - 1] = newInput;
+        newStateInputs = [];
       }
-      // Minus can follow zero or one operand
-      if(/\-/.test(newInputs[0]) &&
-         /[\.\+\-\*\=\/]/.test(lastInput) &&
-         /[\.\+\-\*\=\/]/.test(secondLastInput)) {
-        newInputs = [];
-      }
-      if (newInputs.length !== 0) {
-        newInputs.forEach(newInput => {
+
+      state.isFloat = newStateIsFloat;
+      console.log('final isFloat',isFloat);
+      if (newStateInputs.length !== 0) {
+        newStateInputs.forEach(newInput => {
           inputs.push(newInput)
         });
       }
-
-
     },
     clear: (state, action) => {
       return {
@@ -90,23 +94,7 @@ const inputSlice = createSlice({
 //   }}
 // })
 
-// export const { add, sub, mul, div } = calculationSlice.actions;
-
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched
-// export const incrementAsync = amount => dispatch => {
-//   setTimeout(() => {
-//     dispatch(incrementByAmount(amount));
-//   }, 1000);
-// };
-
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
 export const selectInputs = state => state.input.inputs;
-
 // export const calculateReducer = calculationSlice.reducer;
 export const inputReducer = inputSlice.reducer;
 // export const floatInputReeducer = floatInputSlice.reducer;
