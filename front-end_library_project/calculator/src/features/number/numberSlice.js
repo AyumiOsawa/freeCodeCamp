29603e6588ffccdaa5c2a022996d0716bf12a951
AyumiOsawa <a.osawa1002@gmail.com>
2021-02-15 19:59:56 +0100

    calculation todo added

--- front-end_library_project/calculator/src/features/number/numberSlice.js
+++ front-end_library_project/calculator/src/features/number/numberSlice.js
@@ -1,5 +1,125 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const findOperandIndex = (inputsStr) => {
+  const operandIndexExceptMinus = inputsStr.search(/[\+\*\/]/);
+  const minusIndex = inputsStr.search(/(?<=\d+)\-/);
+  let operandIndex;
+  if (operandIndexExceptMinus === -1 && minusIndex === -1) {
+    operandIndex = false;
+  } else if (operandIndexExceptMinus === -1) {
+    operandIndex = minusIndex;
+  } else {
+    operandIndex = operandIndexExceptMinus;
+  }
+  return operandIndex;
+};
+
+const calculateFormula = (state) => {
+  // TODO: CALCULATE FORMULA RECURSISVELY, UNTIL NO OPERATOR FOUND.
+  let {inputs} = state;
+  inputs = inputs.join('');
+  // get the operand and two numbers (as float)
+  const operandIndex = findOperandIndex(inputs);
+  let firstNum  = parseFloat(inputs.substring(0, operandIndex));
+  let secondNum = parseFloat(inputs.substring(operandIndex + 1));
+  const operand = operandIndex === -1 ? 'no operand given' : inputs[operandIndex];
+   // perform calculation
+  let calculationResult;
+  switch (operand) {
+   case '+':
+     calculationResult = firstNum + secondNum;
+     break;
+   case '-':
+     calculationResult = firstNum - secondNum;
+     break;
+    case '*':
+     calculationResult = firstNum * secondNum;
+     break;
+   case '/':
+     calculationResult = firstNum / secondNum;
+     break;
+   case 'no operand given':
+     calculationResult = inputs;
+     break;
+   default:
+     calculationResult = false;
+  }
+   // The calculationResult is the firstNum, if the last input is an
+   // non-number.
+  const lastInput = inputs[inputs.length - 1];
+  const isTheLastInputOperand = operandIndex === inputs.length - 1;
+  const isTheLastInputDecimalPoint = /[\.]/.test(lastInput);
+  if (isTheLastInputOperand || isTheLastInputDecimalPoint) {
+    calculationResult = firstNum;
+  }
+
+  if (calculationResult) {
+    const resultStr = calculationResult.toString();
+    return {
+      ...state,
+      inputs: resultStr.split(''),
+      isFloat: false
+    };
+  }
+};
+
+const sanitize = (state, action) => {
+  const {inputs, isFloat} = state;
+  const newInput = action.payload;
+  let newStateInputs = [action.payload];
+  let newStateIsFloat = isFloat;
+  // A decimal point can be added only once to one number
+  if (/\./.test(newInput) && isFloat) {
+    newStateInputs = [];
+  } else if (/\./.test(newInput) && !isFloat) {
+    newStateIsFloat = true;
+  }
+  // Float number input mode is reset when a new operand is given
+  if(/[\+\-\*\/]/.test(newInput)) {
+    newStateIsFloat = false;
+  }
+  // Add 0 to the front if the first input was a decimal point
+  if(/\./.test(newInput) && inputs.length === 0) {
+    newStateInputs = ['0', '.'];
+  }
+  // A decimal point cannot follow any operands
+  const lastInput = inputs[inputs.length - 1];
+  const isTheLastInputOperand = /[\+\-\*\/]/.test(lastInput);
+  if (/\./.test(newInput) && isTheLastInputOperand) {
+    newStateInputs = [];
+    newStateIsFloat = false;
+  }
+  // Minus can follow zero or one operand except the decimal point
+  const isTheLastInputDecimalPoint = /\./.test(lastInput);
+  const secondLastInput = inputs[inputs.length - 2];
+  const areTheLast2InputsOperands = /[\.\+\-\*\/]/.test(lastInput) &&
+                                    /[\.\+\-\*\/]/.test(secondLastInput);
+  const shouldRejectMinusInput = areTheLast2InputsOperands ||
+                                 isTheLastInputDecimalPoint;
+  if(/\-/.test(newInput) && shouldRejectMinusInput) {
+    newStateInputs = [];
+  }
+  // Replace the latest input with the new input, if multiple operands were
+  // given in a row.
+  // Replace the second latest input with the new input and delete the last
+  // input, if the second lastest input is also an operand.
+  if (/[\+\*\/]/.test(newInput) && areTheLast2InputsOperands) {
+    // state update
+    state.inputs[state.inputs.length - 2] = newInput;
+    state.inputs.pop();
+    newStateInputs = [];
+  } else if(/[\+\*\/]/.test(newInput) && isTheLastInputOperand) {
+    // state update
+    state.inputs[state.inputs.length - 1] = newInput;
+    newStateInputs = [];
+  }
+  // Operands, except minus, can not be added when the inputs array is empty
+  if (/[\+\*\/]/.test(newInput) &&  inputs.length === 0) {
+    newStateInputs = [];
+  }
+  return {newStateInputs, newStateIsFloat};
+};
+
 const inputSlice = createSlice({
   name: 'input',
   initialState: {
@@ -8,64 +128,12 @@ const inputSlice = createSlice({
   },
   reducers: {
     input: (state, action) => {
-      let {inputs, isFloat} = state;
-      let newInput = action.payload;
-      const lastInput = inputs[inputs.length - 1];
-      const secondLastInput = inputs[inputs.length - 2];
-      let newStateInputs = [action.payload];
-      let newStateIsFloat = isFloat;
-
-      // A decimal point can be added only once to one number
-      if (/\./.test(newInput) && isFloat) {
-        newStateInputs = [];
-      } else if (/\./.test(newInput) && !isFloat) {
-        newStateIsFloat = true;
-      }
-      // Float number input mode is reset when a new operand is given
-      if(/[\+\-\*\/]/.test(newInput)) {
-        newStateIsFloat = false;
-      }
-      // Add 0 to the front if the first input was a decimal point
-      if(/\./.test(newInput) && inputs.length === 0) {
-        newStateInputs = ['0', '.'];
-      }
-      // A decimal point cannot follow any operands
-      const isTheLastInputOperand = /[\+\-\*\/]/.test(lastInput);
-      if (/\./.test(newInput) && isTheLastInputOperand) {
-        newStateInputs = [];
-        newStateIsFloat = false;
-      }
-      // Minus can follow zero or one operand except the decimal point
-      const isTheLastInputDecimalPoint = /\./.test(lastInput);
-      const areTheLast2InputsOperands = /[\.\+\-\*\/]/.test(lastInput) &&
-                                        /[\.\+\-\*\/]/.test(secondLastInput);
-      const shouldRejectMinusInput = areTheLast2InputsOperands ||
-                                     isTheLastInputDecimalPoint;
-      if(/\-/.test(newInput) && shouldRejectMinusInput) {
-        newStateInputs = [];
-      }
-      // Replace the latest input with the new input, if multiple operands were
-      // given in a row.
-      // Replace the second latest input with the new input and delete the last
-      // input, if the second lastest input is also an operand.
-      if (/[\+\*\/]/.test(newInput) && areTheLast2InputsOperands) {
-        inputs[inputs.length - 2] = newInput;
-        inputs.pop();
-        newStateInputs = [];
-      } else if(/[\+\*\/]/.test(newInput) && isTheLastInputOperand) {
-        inputs[inputs.length - 1] = newInput;
-        newStateInputs = [];
-      }
-      // Operands, except minus, can not be added when the inputs array is empty
-      if (/[\+\*\/]/.test(newInput) &&  inputs.length === 0) {
-        newStateInputs = [];
-      }
+      const {newStateInputs, newStateIsFloat} = sanitize(state, action);
       // State update
       state.isFloat = newStateIsFloat;
-      console.log('final isFloat',isFloat);
       if (newStateInputs.length !== 0) {
         newStateInputs.forEach(newInput => {
-          inputs.push(newInput)
+          state.inputs.push(newInput)
         });
       }
     },
@@ -77,71 +145,10 @@ const inputSlice = createSlice({
       };
     },
     calculate: (state, action) => {
-      // Make the inputs array into string, get the index of the operand.
-      // note: if there is no other operand, minus is the main operand.
-      let {inputs} = state;
-      inputs = inputs.join('');
-      const operandIndexExceptMinus = inputs.search(/[\+\*\/]/);
-      const minusIndex = inputs.search(/(?<=\d+)\-/);
-      let operandIndex;
-      let calculationResult;
-
-      if (operandIndexExceptMinus === -1 && minusIndex === -1) {
-        calculationResult = parseFloat(inputs);
-      } else if (operandIndexExceptMinus === -1) {
-        operandIndex = minusIndex;
-      } else {
-        operandIndex = operandIndexExceptMinus;
-      }
-
-       // find the operand and number, and parse each number
-       let firstNum  = parseFloat(inputs.substring(0, operandIndex));
-       let secondNum = parseFloat(inputs.substring(operandIndex + 1));
-       const operand = inputs[operandIndex];
-
-       // perform calculation
-       console.log('inputs', inputs);
-       console.log('operandIndex',operandIndex);
-       console.log('firstNum',firstNum);
-       console.log('operand',operand);
-       console.log('secondNum',secondNum);
-       switch (operand) {
-         case '+':
-           calculationResult = firstNum + secondNum;
-           break;
-         case '-':
-           calculationResult = firstNum - secondNum;
-           break;
-         case '*':
-           calculationResult = firstNum * secondNum;
-           break;
-         case '/':
-           calculationResult = firstNum / secondNum;
-           break;
-         default:
-           calculationResult = false;
-       }
-       // The calculationResult is the firstNum, if the last input is an non
-       // number
-       const lastInput = inputs[inputs.length - 1];
-       const isTheLastInputOperand = operandIndex === inputs.length - 1;
-       const isTheLastInputDecimalPoint = /[\.]/.test(lastInput);
-       if (isTheLastInputOperand || isTheLastInputDecimalPoint) {
-         calculationResult = firstNum;
-       }
-       // State update
-       if (calculationResult) {
-         const resultStr = calculationResult.toString();
-         return {
-           ...state,
-           inputs: resultStr.split(''),
-           isFloat: false
-         };
-       }
+      return calculateFormula(state);
     }
   }
 })
-
 
 export const selectInputs = state => state.input.inputs;
 
